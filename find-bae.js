4e8d25e523623104bcc31d98d5a331a7e2fd7735
mateotis <hekfuszmate@gmail.com>
2019-02-11 23:54:48 +0400

    Leveling system mostly figured out. Also, the cars now work properly??

--- find-bae.js
+++ find-bae.js
@@ -9,7 +9,7 @@ let fakeBaes;
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
-level = 1
+level = 3
 isBaeFound = false
 
 function setup() {
@@ -19,7 +19,7 @@ function setup() {
 
 	charSetup()
 
-	music = loadSound("sounds/music.mp3", playMusic)
+	//music = loadSound("sounds/music.mp3", playMusic)
 	//music.play()
 
 	// cars.forEach(function(element) {
@@ -39,13 +39,13 @@ function draw() {
 
 	// Collision--THAT'S IT
 	player.collide(bae, baeFound);
-	if(level = 2)
+	if(level == 2)
 		player.collide(wall)
 	player.collide(borders)
-	// if(level = 3) {
-	// cars.displace(player)
-	// cars.collide(borders, switchDirections)
-	// }
+	if(level == 3) {
+		cars.displace(player)
+		cars.collide(borders, switchDirections)
+	}
 
 	if(keyDown(LEFT_ARROW))
 		player.velocity.x = -4;
@@ -70,7 +70,7 @@ function draw() {
 		text("Bae found!", width/2, height/2)
 	}
 
-		drawSprites();
+	drawSprites();
 
 	if((isBaeFound) && (keyDown(ENTER))) {
 		nextLevel();
@@ -89,19 +89,40 @@ function baeFound(player, bae){
 function charSetup() {
 
 
-	// Create player
-	player = createSprite(width/2, height/2, 50,50);
+	// Create player; always in the same spot
+	player = createSprite(width/2, 500, 50,50);
 	player.shapeColor = color(0,0,255)
 
-	// Create bae
-	bae = createSprite(random(0, width-50), random(0,height-50), 50,50);
-	bae.shapeColor = color(255,0,0)
+	// Create and place bae
+	if(level == 1) {
+		bae = createSprite(width/2, 300, 50,50);
+		bae.shapeColor = color(255,0,0)
+	}
+	else if(level == 2) {
+		bae = createSprite(width/2, 100, 50,50);
+		bae.shapeColor = color(255,0,0)
+	}
+	else if(level == 3) {
+		bae = createSprite(width/2, 150, 50,50);
+		bae.shapeColor = color(255,0,0)
+	}
+	else if(level == 4) {
+		bae = createSprite(random(0, width-50), random(0,height-50), 50,50);
+		bae.shapeColor = color(255,0,0)
+	}
+	else if(level == 5) {
+		bae = createSprite(width/2, 300, 50,50);
+		bae.shapeColor = color(255,0,0)
+	}	
 
 	console.log(level)
-	console.log(level)
-	console.log("no")
-	wall = createSprite(0, 200, 1500,30);
-	wall.shapeColor = color(0,0,0)
+	if(level == 2) {
+		console.log(level)
+		console.log("no")
+		wall = createSprite(0, 200, 1300,30);
+		wall.shapeColor = color(0,0,0)
+	}
+
 
 	borders = new Group();
 	let westBorder = createSprite(0, 0, 5,1500);
@@ -112,34 +133,37 @@ function charSetup() {
 	eastBorder.shapeColor = color(0,0,0)
 	let southBorder = createSprite(600, 600, 1500,5);
 	southBorder.shapeColor = color(0,0,0)
-	borders.add(westBorder)
-	borders.add(northBorder)
-	borders.add(eastBorder)
-	borders.add(southBorder)
-	// let borderWidth = 0
-	// let borderHeight = 0
-	// for (let i = 0; i < 4; i++){
-	// 	let border = createSprite(0, 0, 5,1500)
-	// }
+	if(level != 2) {
+		borders.add(westBorder)
+		borders.add(northBorder)
+		borders.add(eastBorder)
+		borders.add(southBorder)
+	}
 
-	// cars = new Group();
-	// let cnt = 0
-	// for (let i = 0; i < 4; i++){
-	// 	let car = createSprite(70, 500 - cnt, 70, 50);
-	// 	car.shapeColor = color(0,255,0)
-	// 	cars.add(car);
-	// 	console.log(car.position)
-	// 	cnt += 100
-	// }
-	// cnt = 0
+	if(level == 3) {
+		cars = new Group();
+		let cnt = 0
+		for (let i = 0; i < 4; i++){
+			let car = createSprite(70, 500 - cnt, 70, 50);
+			car.shapeColor = color(0,255,0)
+			cars.add(car);
+			console.log(car.position)
+			cnt += 100
+		}
+		cnt = 0
+		cars.forEach(function(car) {
+			car.velocity.x = 5;
+		})
+	}
 
-	// fakeBaes = new Group();
-	// for (let i = 0; i < 30; i++){
-	// 	let fakeBae = createSprite(random(0,width), random(0,height), 50, 50);
-	// 	fakeBae.shapeColor = color(230,0,0)
-	// 	fakeBaes.add(fakeBae);
-	// }
-
+	if(level == 4) {
+		fakeBaes = new Group();
+		for (let i = 0; i < 30; i++){
+			let fakeBae = createSprite(random(0,width), random(0,height), 50, 50);
+			fakeBae.shapeColor = color(230,0,0)
+			fakeBaes.add(fakeBae);
+	}
+	}
 
 
 }
@@ -147,9 +171,22 @@ function charSetup() {
 function nextLevel() {
 	console.log("in nextlevel")
 	isBaeFound = false
-	level += 1
 	player.remove()
 	bae.remove()
+	if(level == 2)
+		wall.remove();
+	if(level == 3)
+		console.log(cars)
+		cars.length = 0
+		console.log(cars)
+	if(level == 4) {
+		fakeBaes.forEach(function(fakeBae) {
+			fakeBae.remove()
+			console.log('removed a fakebae')
+		});
+
+	}
+	level += 1
 	charSetup()
 }
 
